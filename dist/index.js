aefa2782b6f028cbdb3bb2b4fd7429c251b2216f
Randy Gulak <randy@randygulak.com>
2022-04-10 09:42:35 -0700

    creating basic types

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,9 @@
 "use strict";
+// Basic Types
 let id = 5;
+let myName = "Randy";
+let isDeveloper = true;
+// like vanilla JS, can be assigned to whatever
+let x = "hello";
+// for an array that only contains numbers
+let ids = [1, 2, 3, 4, 5];
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+// Basic Types
+
 let id: number = 5;
 
+let myName: string = "Randy";
 
+let isDeveloper: boolean = true;
+
+// like vanilla JS, can be assigned to whatever
+let x: any = "hello";
+
+// for an array that only contains numbers
+let ids: number[] = [1, 2, 3, 4, 5];
